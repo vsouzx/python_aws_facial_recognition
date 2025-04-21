a808071a19d3ff8fb34bd746f640850424b74a6a
vsouzx_ <vtsoliveira2001@gmail.com>
2025-04-21 19:17:28 -0300

    teste cors

--- infra/gtw.tf
+++ infra/gtw.tf
@@ -199,7 +199,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
     rest_api_id = aws_api_gateway_rest_api.facial_recognition_gw_api.id
 
     triggers = {
-      redeployment = timestamp()
+      redeployment = "${timestamp()}-${filebase64sha256("${path.module}/lambda.zip")}"
     }
 
     lifecycle {
